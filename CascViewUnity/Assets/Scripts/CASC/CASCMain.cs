cab56f79c003e2d7734ab0bfb4723afc13e811b8
Selzier <ryan.zehm@gmail.com>
2021-03-13 15:50:38 -0800

    Have created a Mesh() from BLP data
    Texture2d() from a BLP
    Mesh() from an M2

--- CascViewUnity/Assets/Scripts/CASC/CASCMain.cs
+++ CascViewUnity/Assets/Scripts/CASC/CASCMain.cs
@@ -8,26 +8,11 @@ using System.Linq;
 using SimpleSQL;
 using M2Lib;
 using M2Lib.m2;
+using UnityEngine.Rendering;
 
 public class CASCMain : MonoBehaviour
 {
-    /// <summary>
-    ///     Versions of M2 encountered so far.
-    /// </summary>
-    public enum M2Format
-    {
-        Useless = 0xCAFE,
-        Classic = 256,
-        BurningCrusade = 260,
-        LateBurningCrusade = 263,
-        LichKing = 264,
-        Cataclysm = 272,
-        Pandaria = 272,
-        Draenor = 272,
-        Legion = 274
-    }
-
-    public enum ChunkID
+    /*public enum ChunkID
     {
         MD20 = 808600653,
         MD21 = 825377869,
@@ -55,7 +40,7 @@ public class CASCMain : MonoBehaviour
         EDGF = 1179075653,
         NERF = 1179796814,
         DETL = 1280591172
-    }
+    }*/
 
     public string selectedPath;
     public string listFilePath;
@@ -73,20 +58,15 @@ public class CASCMain : MonoBehaviour
     public static List<CASCFile> CASCFiles;
     public static List<CASCFolder> CASCFolders;
 
-    private BinaryReader br; // Reading M2 Files, etc
-
-    private void OnEnable()
-    {
+    private void OnEnable() {
         //CASCFiles = new List<CASCFile>();
         CASCFiles = new List<CASCFile>();
         dbManager = GetComponent<SimpleSQLManager>();
-    }    
+    }
 
-    IEnumerator Start()
-    {
-        if (outputPath == "")
-        {
-            outputPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"\\ExtractedCSACPath\\";
+    IEnumerator Start() {
+        if (outputPath == "") {
+            outputPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\ExtractedCSACPath\\";
         }
         OpenStorage();
         yield return null;
@@ -103,17 +83,23 @@ public class CASCMain : MonoBehaviour
         //ExtractAllFiles(fileToExport);
 
         // Mesh: 1859623
-        Mesh m2Mesh = M2ToMesh(GetCascFile(imageFileDataID));
+
+        (Mesh m2Mesh, Texture2D[] textures) = M2ToMesh(GetCascFile(imageFileDataID));
         MeshFilter meshFilter = GetComponent<MeshFilter>();
         meshFilter.sharedMesh = m2Mesh;
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        renderer.material = testMaterial;
+        testMaterial.mainTexture = textures[1];
+
+
 
         // Texture: 158918
         //Texture2D blpTexture = BlpToTexture2d(GetCascFile(imageFileDataID));
         //testMaterial.SetTexture("_MainTex", blpTexture);
-        
+
         yield return null;
     }
-    
+
     public ListFileStructured GetFileInfoFromDB(int fileDataID) {
         List<ListFileStructured> match = CASCMain.dbManager.Query<ListFileStructured>("SELECT * FROM files WHERE FileDataID = " + fileDataID + ";");
         if (match.Count > 0) {
@@ -137,16 +123,13 @@ public class CASCMain : MonoBehaviour
         }
     }
 
-    private void OpenStorage()
-    {
+    private void OpenStorage() {
         CASCFiles.Clear();
         storage?.Dispose();
-        try
-        {
+        try {
             storage = new CASCStorage(selectedPath, listFilePath, csvFilePath);
         }
-        catch(Exception e)
-        {
+        catch (Exception e) {
             storage?.Dispose();
             Debug.Log(e);
             Debug.LogError("Failed to open CASC Storage. Ensure that the storage is intact and exists at the given location.");
@@ -158,11 +141,9 @@ public class CASCMain : MonoBehaviour
     /// Exports native CASC files, for example this will export BLP files, not PNGs.
     /// </summary>
     /// <param name="FileList">List of files to be extracted</param>
-    private void ExtractAllFiles(List<CASCFile> FileList)
-    {
+    private void ExtractAllFiles(List<CASCFile> FileList) {
         var files = FileList;
-        if (files.Count == 0)
-        {
+        if (files.Count == 0) {
             Debug.Log("No files provided to extract.");
             return;
         }
@@ -170,68 +151,9 @@ public class CASCMain : MonoBehaviour
         StartCoroutine(ExtractCoroutine(FileList));
     }
 
-    private void ParseChunkMD21() {
-        long positionMD21Begin = br.BaseStream.Position;
-        
-        ChunkID chunkID = (ChunkID)br.ReadUInt32();
-        if (chunkID != ChunkID.MD20) {
-            Debug.LogError("Invalid M2 Magic: " + chunkID);
-        }
-        M2Format version = (M2Format)br.ReadUInt32();
-        Debug.Log("Version: " + version);
-        
-        // Move to function ParseMD21ModelName ?
-        int modelNameLength = (int)br.ReadUInt32();
-        int modelNameOffset = (int)br.ReadUInt32();
-        
-        long position = br.BaseStream.Position; // Save current position
-
-        br.BaseStream.Seek(positionMD21Begin + modelNameOffset, SeekOrigin.Begin);
-        string name = System.Text.Encoding.UTF8.GetString(br.ReadBytes(modelNameLength));
-        Debug.Log("Model Name: " + name);
-
-        //br.BaseStream.Seek(position, SeekOrigin.Begin);
-        // ??
-
-        br.BaseStream.Seek(position + 4 + 8 + 8 + 8, SeekOrigin.Begin);
-
-        // parseChunk_MD21_bones
-        int boneCount = (int)br.ReadUInt32();
-        int boneOffset = (int)br.ReadUInt32();
-        Debug.Log("Bone Count: " + boneCount);
-
-        position = br.BaseStream.Position;
-
-        br.BaseStream.Seek(positionMD21Begin + boneOffset, SeekOrigin.Begin);
-
-        for (int i = 0; i < boneCount; i++) {
-            int boneID = (int)br.ReadInt32();
-            int flags = (int)br.ReadUInt32();
-            int parentBone = (int)br.ReadInt16();
-            int subMeshID = (int)br.ReadUInt16();
-            int boneNameCRC = (int)br.ReadUInt32();
-        }
-
-
-    }
-
-    private void ParseChunkSFID() {
-        int[] skinFileDataIDs = new int[1]; // FIX ME
-        for (int i = 0; i < 1; i++) { // FIX ME
-            skinFileDataIDs[i] = (int)br.ReadUInt32();
-        }
-        foreach (int skinID in skinFileDataIDs) {
-            Debug.Log(skinID);
-        }
-    }
-    
-    private void ParseChunkTXID() {
-
-    }
-
-    public Mesh M2ToMesh(CASCFile file) {
+    public (Mesh, Texture2D[]) M2ToMesh(CASCFile file) {
         Mesh m2Mesh = new Mesh();
-        br = new BinaryReader(new MemoryStream(CascFileBytes(file))); // New BinaryReader for the file in MemoryStream
+        BinaryReader br = new BinaryReader(new MemoryStream(CascFileBytes(file))); // New BinaryReader for the file in MemoryStream
         br.BaseStream.Seek(0, SeekOrigin.Begin);    // Go the the beginning of the stream
 
         // Manually Parsing the File             
@@ -258,31 +180,34 @@ public class CASCMain : MonoBehaviour
             br.BaseStream.Seek(nextChunkPos, SeekOrigin.Begin);  // Advance the reader position to the the next chunk and proceed with loop
             Debug.Log("Processed ChunkID: " + chunkID);          // Name of current chunk
         }*/
-        
+
         var model = new M2();
         model.Load(br);
 
-        Debug.Log(model.Name + " loaded");
-        Debug.Log(model.Version);
-        Debug.Log(model.nViews);
-        Debug.Log(model.Textures.Count);
+        //Debug.Log(model.Name + " loaded");
+        //Debug.Log(model.Version);
+        Debug.Log("nViews: " + model.nViews);
+        Debug.Log("Texture Count: " + model.Textures.Count);
+        Texture2D[] textures = new Texture2D[model.Textures.Count];
         for (int i = 0; i < model.Textures.Count; i++) {
-            Debug.Log(model.Textures[i].Name);
+            Debug.Log("Texture File: " + model.Textures[i].Name + "FileDataID: " + model.Textures[i].FileDataID);
+            Texture2D tex = BlpToTexture2d(GetCascFile(model.Textures[i].FileDataID));
+            textures[i] = tex;
         }
-        foreach (int i in model.skinFileDataIDs) {
-            Debug.Log("Skin file: " + i);
-            //skinFile.
-            var view = new M2SkinProfile();
-            //var substream = stream.BaseStream as Substream;
-            //var path = substream != null ? ((FileStream)substream.GetInnerStream()).Name : ((FileStream)stream.BaseStream).Name;
-            //using (var skinFile = new BinaryReader(new FileStream(M2SkinProfile.SkinFileName(path, i), FileMode.Open)))
-            using (var skinFile = new BinaryReader(new MemoryStream(CascFileBytes(GetCascFile(i))))) {
-                view.Load(skinFile, model.Version);
-                view.LoadContent(skinFile, model.Version);
+
+        if (model.Views.Count > 0) {
+            for (int i = 0; i < model.Views.Count; i++) {
+                //var substream = stream.BaseStream as Substream;
+                //var path = substream != null ? ((FileStream)substream.GetInnerStream()).Name : ((FileStream)stream.BaseStream).Name;
+                //using (var skinFile = new BinaryReader(new FileStream(M2SkinProfile.SkinFileName(path, i), FileMode.Open)))
+                using (var skinFile = new BinaryReader(new MemoryStream(CascFileBytes(GetCascFile(model.Views[i].FileDataID))))) {
+                    model.Views[i].Load(skinFile, model.Version);
+                    model.Views[i].LoadContent(skinFile, model.Version);
+                }
+                Debug.Log("Skin File: " + model.Views[i].FileDataID);
             }
-            model.Views.Add(view);
         }
-        
+
         Vector3[] vertices = new Vector3[model.GlobalVertexList.Count];
         Vector3[] normals = new Vector3[model.GlobalVertexList.Count];
         Vector2[] uv1 = new Vector2[model.GlobalVertexList.Count];
@@ -301,13 +226,29 @@ public class CASCMain : MonoBehaviour
         for (int i = 0; i < model.Views[0].Triangles.Count; i++) {
             triangles[i] = (int)model.Views[0].Triangles[i];
         }
-        
+
+        // Compltely Wrong
+        /*
+        for (int i = 0; i < model.Views[0].Submeshes.Count; i++) {
+            M2SkinSection sub = model.Views[0].Submeshes[i];
+            SubMeshDescriptor smd = new SubMeshDescriptor();
+            smd.baseVertex = sub.StartVertex;
+            Bounds bounds = new Bounds(sub.CenterBoundingBox, new Vector3(sub.Radius, sub.Radius, sub.Radius)); // radius isn't correct
+            smd.bounds = bounds;
+            smd.firstVertex = 0; ///?
+            smd.indexCount = 3;
+            smd.indexStart = sub.StartVertex;
+            smd.topology = MeshTopology.Triangles;
+            smd.vertexCount = sub.NVertices;
+            m2Mesh.SetSubMesh(i, smd, MeshUpdateFlags.Default);
+        }*/
+
         m2Mesh.vertices = vertices;
         m2Mesh.normals = normals;
         m2Mesh.triangles = triangles.Reverse().ToArray(); // Reverses the faces
         m2Mesh.uv = uv1;
         m2Mesh.uv2 = uv2;
-        return m2Mesh;
+        return (m2Mesh, textures);
     }
 
     /// <summary>
@@ -315,7 +256,7 @@ public class CASCMain : MonoBehaviour
     /// </summary>
     /// <param name="file"></param>
     public Texture2D BlpToTexture2d(CASCFile file) {
-        Texture2D blpTex = null;        
+        Texture2D blpTex = null;
         using (var blp = new BlpFile(new MemoryStream(CascFileBytes(file)))) {
             blpTex = blp.GetTexture2d(0); // getting mipmap 0, TODO: get all mipmaps
         }
@@ -360,7 +301,7 @@ public class CASCMain : MonoBehaviour
         catch (Exception ex) {
             Debug.LogError(ex);
             return null;
-        }        
+        }
     }
 
     /// <summary>
@@ -368,69 +309,58 @@ public class CASCMain : MonoBehaviour
     /// </summary>
     /// <param name="files"></param>
     /// <returns></returns>
-    IEnumerator ExtractCoroutine(List<CASCFile> files)
-    {
+    IEnumerator ExtractCoroutine(List<CASCFile> files) {
         Debug.Log("Number of files to extract:" + files.Count);
         int filesProcessed = 0;
-        foreach (var file in files)
-        {
+        foreach (var file in files) {
             yield return null;
-            if (file.IsLocal == "Yes")
-            {
+            if (file.IsLocal == "Yes") {
                 string outputLocation = outputPath + file.FullPath.Replace("\0", String.Empty); /// For saving the files such that it respect the directory
                 //string outputLocation = outputPath + ReplaceInvalidChars(file.FileName);      /// For saving the files without directory...using for testing purposes
                 Directory.CreateDirectory(Path.GetDirectoryName(outputLocation));
-                try
-                {
+                try {
                     var input = storage.OpenFile(file.FullPath);
                     using (var output = File.Create(outputLocation, 128, FileOptions.SequentialScan)) {
                         System.IntPtr pspanPointer;
                         CASCFunc.CASC_FILE_FULL_INFO fileInfo;
-                        if (input.GetInfo(out fileInfo, out pspanPointer))
-                        {
-                            for (int i = 0; i < fileInfo.SpanCount; i++)
-                            {
-                                var pspan = Marshal.PtrToStructure<CASCFunc.CASC_FILE_SPAN_INFO>(IntPtr.Add(pspanPointer,i*Marshal.SizeOf(typeof(CASCFunc.CASC_FILE_SPAN_INFO))));
+                        if (input.GetInfo(out fileInfo, out pspanPointer)) {
+                            for (int i = 0; i < fileInfo.SpanCount; i++) {
+                                var pspan = Marshal.PtrToStructure<CASCFunc.CASC_FILE_SPAN_INFO>(IntPtr.Add(pspanPointer, i * Marshal.SizeOf(typeof(CASCFunc.CASC_FILE_SPAN_INFO))));
                                 int cbFileSpan = (int)(pspan.EndOffset - pspan.StartOffset);
                                 if (cbFileSpan == 0)
                                     continue;
                                 byte[] buffer;
                                 int bytesRead;
-                                bool readOK= input.Read(out buffer, cbFileSpan,out bytesRead);
-                                if (readOK)
-                                {
+                                bool readOK = input.Read(out buffer, cbFileSpan, out bytesRead);
+                                if (readOK) {
                                     output.Write(buffer, 0, bytesRead);
                                     if (bytesRead < cbFileSpan)
                                         break;
                                 }
-                                else
-                                {
+                                else {
                                     break;
                                 }
                             }
                         }
                     }
                 }
-                catch (Exception ex)
-                {
+                catch (Exception ex) {
                     Debug.LogError(ex);
                 }
             }
             filesProcessed++;
-            Debug.Log("Progress:" + filesProcessed + "/" + files.Count+" FileName:"+file.FullPath);
+            Debug.Log("Progress:" + filesProcessed + "/" + files.Count + " FileName:" + file.FullPath);
         }
         yield return null;
     }
 
-    private void OnDisable()
-    {
+    private void OnDisable() {
         CASCFiles.Clear();
         dbManager.Dispose();
         dbManager.Close();
     }
 
-    string ReplaceInvalidChars(string filename)
-    {
+    string ReplaceInvalidChars(string filename) {
         filename.TrimEnd();
         return string.Join("", filename.Split(Path.GetInvalidFileNameChars()));
     }
